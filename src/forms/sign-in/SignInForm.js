c7af740b8ab3e6d0e83a0e555093e0bd4c27ef66
SRRayhan066 <shafikulrahman66@gmail.com>
2025-11-15 12:38:23 +0600

    Implement sign in form

--- src/forms/sign-in/SignInForm.js
+++ src/forms/sign-in/SignInForm.js
@@ -3,21 +3,20 @@
 import Input from "@/components/ui/input/Input";
 import Button from "@/components/ui/button/Button";
 
-export default function SignUpForm({ onToggle = () => {} }) {
+export default function SignInForm({ onToggle = () => {} }) {
   return (
     <div className="w-full max-w-2xl flex flex-col items-center gap-3 p-6 bg-neutral-900 rounded-lg shadow-md">
-      <h1 className="text-3xl font-bold text-slate-200 mb-2">Sign Up</h1>
+      <h1 className="text-3xl font-bold text-slate-200 mb-2">Sign In</h1>
       <p className="text-slate-400 mb-6 text-center">
-        Create an account to get started.
+        Sign in to your account to continue.
       </p>
-      <Input placeholder="Name" type="text" />
       <Input placeholder="Email" type="email" />
       <Input placeholder="Password" type="password" />
-      <Button className={"w-32"}>Sign Up</Button>
-      <p>
-        Already have an account?{" "}
+      <Button className={"w-32"}>Sign In</Button>
+      <p className="">
+        Don't have an account?{" "}
         <span className="font-bold cursor-pointer" onClick={onToggle}>
-          Log In
+          Register
         </span>
       </p>
     </div>
--- src/sections/auth-section/AuthSection.js
+++ src/sections/auth-section/AuthSection.js
@@ -1,9 +1,23 @@
+"use client";
+
+import { useState } from "react";
 import SignUpForm from "@/forms/sign-up/SignUpForm";
+import SignInForm from "@/forms/sign-in/SignInForm";
 
 export default function AuthSection() {
+  const [isSignIn, setIsSignIn] = useState(true);
+
+  const toggleForm = () => {
+    setIsSignIn(!isSignIn);
+  };
+
   return (
     <section className="flex justify-center items-center w-full h-screen">
-      <SignUpForm />
+      {isSignIn ? (
+        <SignInForm onToggle={toggleForm} />
+      ) : (
+        <SignUpForm onToggle={toggleForm} />
+      )}
     </section>
   );
 }
